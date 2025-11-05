0e640450ca59bf620dc08d711dbdde01b1e43942
Julia Salgarolo <jula.salgarolo@gmail.com>
2025-11-05 16:01:18 +0000

    Operational Margin per Order calculation

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,26 @@
+-- Operational_margin = margin + shipping_fee - log_cost - ship_cost
+
+with operational_margin_selected as (
+select  orders_id,
+        date_date,
+        margin,
+        quantity,
+        shipping_fee,
+        logcost,
+        ship_cost
+from {{ ref('int_orders_margin') }} 
+join {{ ref('stg_raw__ship') }} 
+    using (orders_id)
+)
+
+select  orders_id,
+        date_date,
+        SUM(shipping_fee) as shipping_fee,
+        SUM(logcost) as logcost,
+        SUM(ship_cost) as ship_cost,
+        SUM(quantity) as quantity,
+        SUM((margin + shipping_fee) - (logcost - ship_cost)) as operational_margin
+from operational_margin_selected
+group by orders_id,
+         date_date 
+order by orders_id
