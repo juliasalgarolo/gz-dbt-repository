6cb2dacaa7835aabccce4013c4c1f0698519414e
Julia Salgarolo <jula.salgarolo@gmail.com>
2025-11-06 14:15:24 +0000

    int_campaigns_day done

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,3 +1,7 @@
-select *
+select  date_date,
+        SUM(ads_cost) as ads_cost,
+        SUM(impressions) as impressions,
+        SUM(clicks) as clicks
 from {{ ref('int_campaigns') }}
-group by date_date
+group by date_date 
+order by date_date desc
