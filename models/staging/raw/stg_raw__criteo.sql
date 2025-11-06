f461e5dae4c245619cfc0feb2ba0adad40e7d156
Julia Salgarolo <jula.salgarolo@gmail.com>
2025-11-06 11:42:59 +0000

    added 4 new databases

---
+++ models/staging/raw/stg_raw__criteo.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'criteo') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        paid_source,
+        campaign_key,
+        campgn_name as campaign_name,
+        cast(ads_cost as float64) as ads_cost,
+        cast(impression as int64) as impressions,
+        cast(click as int64) as clicks
+    from source
+
+)
+
+select * from renamed
